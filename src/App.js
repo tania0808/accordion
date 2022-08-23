be1f3850c142c8f0725d2e85b9c13494688815f9
Tania <tania@MacBook-Pro-de-nico.local>
2022-08-23 14:15:44 +0200

    finished page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   return (
     <div className="box">
       <div className="container">
+        <h1 className="heading">Questions and answers about login</h1>
+        <div className="accordion">
+          {
+            data.map(item => {
+              return <SingleQuestion key={item.id} {...item}/>
+            })
+          }
+        </div>
         <div className="rectangle"></div>
         <div className="angle"></div>
       </div>
-      ;
     </div>
   );
 }
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleButton = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className="question-box">
+      <div className="question">
+        <h4>{title}</h4>
+        <p className={isOpen ? "d-block" : "d-hidden"}>{info}</p>
+      </div>
+      <button className="btn" onClick={toggleButton}>
+        <p className="sign">{isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/> }</p>
+      </button>
+    </div>
+  );
 };
 
 export default Question;
