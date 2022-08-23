8290230af72aed64a518341dd1747fac8660780a
Tania <tania@MacBook-Pro-de-nico.local>
2022-08-23 14:21:54 +0200

    fixed description position

--- src/Question.js
+++ src/Question.js
@@ -9,11 +9,11 @@ const Question = ({ title, info }) => {
     <div className="question-box">
       <div className="question">
         <h4>{title}</h4>
-        <p className={isOpen ? "d-block" : "d-hidden"}>{info}</p>
-      </div>
       <button className="btn" onClick={toggleButton}>
         <p className="sign">{isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/> }</p>
       </button>
+      </div>
+        <p className={isOpen ? "d-block" : "d-hidden"}>{info}</p>
     </div>
   );
 };
